a950abcaccef77bbef06ab0e26f59b011487a909
jacob <jacco_mb@mailbox.org>
2024-10-24 17:10:43 +0200

    Reorder the files and rename files

--- main.py
+++ main.py
@@ -10,133 +10,143 @@ from sklearn.metrics import root_mean_squared_error
 from sklearn.utils import shuffle
 import matplotlib.pyplot as plt
 
-#def main():
+def main():
 
-#set epochs
-epochs = 50
-trial_size = 900
+    #get number of epochs
+    epochs = get_epochs()
 
-# Load the data
-data_train = pd.read_csv('new_train.csv', sep=';')
-data_test = pd.read_csv('new_test.csv', sep=';')
 
-# Split the data into features and target
-X = data_train.iloc[:, :-1].values  # All columns except the last one
-y = data_train.iloc[:, 11].values   # 12th column (index 11)
+    #set epochs
+    epochs = 50
 
-# Split the data into training+validation and testing sets
-#X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.0, random_state=42)
-X_train_val = X
-y_train_val = y
-X_test = data_test.iloc[:, :-1].values
-y_test = data_test.iloc[:, 11].values
+    # Load the data
+    data_train = pd.read_csv('new_train.csv', sep=';')
+    data_test = pd.read_csv('new_test.csv', sep=';')
 
-# Generate trial IDs
-num_datapoints = len(X_train_val)
-num_trials = num_datapoints // trial_size
-trial_ids = np.repeat(np.arange(num_trials), trial_size)
+    # Split the data into features and target
+    X = data_train.iloc[:, :-1].values  # All columns except the last one
+    y = data_train.iloc[:, 11].values   # 12th column (index 11)
 
-# Shuffle the data while keeping trials intact
-X_train_val, y_train_val, trial_ids = shuffle(X_train_val, y_train_val, trial_ids, random_state=42)
+    # Split the data into training+validation and testing sets
+    #X_train_val, X_test, y_train_val, y_test = train_test_split(X, y, test_size=0.0, random_state=42)
+    X_train_val = X
+    y_train_val = y
+    X_test = data_test.iloc[:, :-1].values
+    y_test = data_test.iloc[:, 11].values
 
-# Standardize the data
-scaler = StandardScaler()
-X_train_val = scaler.fit_transform(X_train_val)
-X_test = scaler.transform(X_test)
+    # Generate trial IDs
+    num_datapoints = len(X_train_val)
+    num_trials = num_datapoints // trial_size
+    trial_ids = np.repeat(np.arange(num_trials), trial_size)
 
-# Implement 5-fold cross-validation on the training+validation set
-gkf = GroupKFold(n_splits=5)#, shuffle=False)#, random_state=42)
-fold = 1
-val_losses = []
-rmses = []
-best_val_loss = np.inf
-best_model_weights = None
+    # Shuffle the data while keeping trials intact
+    X_train_val, y_train_val, trial_ids = shuffle(X_train_val, y_train_val, trial_ids, random_state=42)
 
-for train_index, val_index in gkf.split(X_train_val, y_train_val, groups=trial_ids):
-    print()
-    print(f"===Fold {fold}===")
-    X_train, X_val = X_train_val[train_index], X_train_val[val_index]
-    y_train, y_val = y_train_val[train_index], y_train_val[val_index]
+    # Standardize the data
+    scaler = StandardScaler()
+    X_train_val = scaler.fit_transform(X_train_val)
+    X_test = scaler.transform(X_test)
 
-    # Build the model
-    model = Sequential()
-    model.add(Dense(11, activation='relu', input_shape=(X_train.shape[1],)))
-    model.add(Dense(300, activation='relu', kernel_initializer='HeNormal'))
-    model.add(Dropout(0.2))
-    model.add(Dense(60, activation='relu', kernel_initializer='HeNormal'))
-    model.add(Dropout(0.2))
-    model.add(Dense(1))
+    # Implement 5-fold cross-validation on the training+validation set
+    gkf = GroupKFold(n_splits=5)#, shuffle=False)#, random_state=42)
+    fold = 1
+    val_losses = []
+    rmses = []
+    best_val_loss = np.inf
+    best_model_weights = None
 
-    # Compile the model
-    optimizer = Adam(learning_rate=0.005, beta_1=0.9, beta_2=0.999)
-    model.compile(optimizer=optimizer, loss='mean_absolute_error')
-    
-    # early stopping and reset the weights to the best model with the lowest validation loss
-    early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=6, restore_best_weights=True) 
-    #saves the best model of the fold based on the validation loss (including weights)
-    checkpoint = tf.keras.callbacks.ModelCheckpoint(f'best_model_{fold}.keras', monitor='val_loss', save_best_only=True, mode='min', verbose=1)
+    for train_index, val_index in gkf.split(X_train_val, y_train_val, groups=trial_ids):
+        print()
+        print(f"===Fold {fold}===")
+        X_train, X_val = X_train_val[train_index], X_train_val[val_index]
+        y_train, y_val = y_train_val[train_index], y_train_val[val_index]
 
-    # Train the model
-    model.fit(X_train, y_train, epochs=epochs, batch_size=10, validation_data=(X_val, y_val), callbacks=[early_stopping, checkpoint])
+        # Build the model
+        model = Sequential()
+        model.add(Dense(11, activation='relu', input_shape=(X_train.shape[1],)))
+        model.add(Dense(300, activation='relu', kernel_initializer='HeNormal'))
+        model.add(Dropout(0.2))
+        model.add(Dense(60, activation='relu', kernel_initializer='HeNormal'))
+        model.add(Dropout(0.2))
+        model.add(Dense(1))
 
-    # Evaluate the model
+        # Compile the model
+        optimizer = Adam(learning_rate=0.005, beta_1=0.9, beta_2=0.999)
+        model.compile(optimizer=optimizer, loss='mean_absolute_error')
+        
+        # early stopping and reset the weights to the best model with the lowest validation loss
+        early_stopping = tf.keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=6, restore_best_weights=True) 
+        #saves the best model of the fold based on the validation loss (including weights)
+        checkpoint = tf.keras.callbacks.ModelCheckpoint(f'best_model_{fold}.keras', monitor='val_loss', save_best_only=True, mode='min', verbose=1)
 
-    model.load_weights(f'best_model_{fold}.keras')
-    val_loss = model.evaluate(X_val, y_val)
-    val_losses.append(val_loss)
-    print(f'Fold {fold} - Validation loss: {val_loss}')
+        # Train the model
+        model.fit(X_train, y_train, epochs=epochs, batch_size=10, validation_data=(X_val, y_val), callbacks=[early_stopping, checkpoint])
 
-    # Predict the validation set results
-    y_pred = model.predict(X_val)
+        # Evaluate the model
 
-    # Calculate RMSE
-    rmse = np.sqrt(root_mean_squared_error(y_val, y_pred))
-    rmses.append(rmse)
-    print(f'Fold {fold} - RMSE: {rmse}')
+        model.load_weights(f'best_model_{fold}.keras')
+        val_loss = model.evaluate(X_val, y_val)
+        val_losses.append(val_loss)
+        print(f'Fold {fold} - Validation loss: {val_loss}')
 
-    fold += 1
+        # Predict the validation set results
+        y_pred = model.predict(X_val)
 
-# Calculate average validation loss and RMSE
-avg_val_loss = np.mean(val_losses)
-avg_rmse = np.mean(rmses)
-print(f'Average Validation Loss: {avg_val_loss}')
-print(f'Average RMSE: {avg_rmse}')
-#best fold
-best_fold_loss = np.argmin(val_losses) + 1
-print(f'Best Fold according to loss: {best_fold_loss}')
-best_fold_rmse = np.argmin(rmses) + 1
-print(f'Best Fold according to RMSE: {best_fold_rmse}')
+        # Calculate RMSE
+        rmse = np.sqrt(root_mean_squared_error(y_val, y_pred))
+        rmses.append(rmse)
+        print(f'Fold {fold} - RMSE: {rmse}')
 
-# Load the best weights from cross-validation
-model.load_weights(f'best_model_{best_fold_loss}.keras')
+        fold += 1
 
-# Evaluate the model on the test set
-test_loss = model.evaluate(X_test, y_test)
-print(f'Test Loss: {test_loss}')
-y_test_pred = model.predict(X_test)
-test_rmse = np.sqrt(root_mean_squared_error(y_test, y_test_pred))
-print(f'Test RMSE: {test_rmse}')
+    # Calculate average validation loss and RMSE
+    avg_val_loss = np.mean(val_losses)
+    avg_rmse = np.mean(rmses)
+    print(f'Average Validation Loss: {avg_val_loss}')
+    print(f'Average RMSE: {avg_rmse}')
+    #best fold
+    best_fold_loss = np.argmin(val_losses) + 1
+    print(f'Best Fold according to loss: {best_fold_loss}')
+    best_fold_rmse = np.argmin(rmses) + 1
+    print(f'Best Fold according to RMSE: {best_fold_rmse}')
 
-'''
-# Plot loss and val_loss over the epochs for the last fold
-plt.plot(model.history['loss'], label='Training Loss')
-plt.plot(model.history['val_loss'], label='Validation Loss')
-plt.title('Model Loss')
-plt.xlabel('Epochs')
-plt.ylabel('Loss')
-plt.legend()
-plt.show()
-'''
+    # Load the best weights from cross-validation
+    model.load_weights(f'best_model_{best_fold_loss}.keras')
 
-# Plot y_pred and y_test as a dot plot for the test set
-plt.figure(figsize=(10, 6))
-plt.scatter(y_test, y_test_pred, alpha=0.5, label='Predicted vs Actual')
-plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red', label='Ideal Line')
-plt.xlabel('Actual Values')
-plt.ylabel('Predicted Values')
-plt.title('Actual vs Predicted Values')
-plt.legend()
-plt.show()
+    # Evaluate the model on the test set
+    test_loss = model.evaluate(X_test, y_test)
+    print(f'Test Loss: {test_loss}')
+    y_test_pred = model.predict(X_test)
+    test_rmse = np.sqrt(root_mean_squared_error(y_test, y_test_pred))
+    print(f'Test RMSE: {test_rmse}')
 
-#if __name__ == "__main__":
-#    print("Test_BA_Data.py is being run directly")
+    '''
+    # Plot loss and val_loss over the epochs for the last fold
+    plt.plot(model.history['loss'], label='Training Loss')
+    plt.plot(model.history['val_loss'], label='Validation Loss')
+    plt.title('Model Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+    plt.show()
+    '''
+
+    # Plot y_pred and y_test as a dot plot for the test set
+    plt.figure(figsize=(10, 6))
+    plt.scatter(y_test, y_test_pred, alpha=0.5, label='Predicted vs Actual')
+    plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red', label='Ideal Line')
+    plt.xlabel('Actual Values')
+    plt.ylabel('Predicted Values')
+    plt.title('Actual vs Predicted Values')
+    plt.legend()
+    plt.show()
+
+def get_epochs():
+    while True:
+        try:
+            return int(input("Enter the number of epochs: "))
+        except ValueError:
+            pass
+
+if __name__ == "__main__":
+    print("Test_BA_Data.py is being run directly")
