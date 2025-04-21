4c787b09fba573ff181c419d2ef9157dcfca3a4c
jacob <jacco_mb@mailbox.org>
2025-04-21 20:07:06 +0200

    ReadMe.txt and other adjustments

--- postprocessing/Heatmap_Hist_Duration_Height.py
+++ postprocessing/Heatmap_Hist_Duration_Height.py
@@ -1,41 +1,46 @@
 import os
+# this file is used to plot some heatmaps and histograms from the participants data
 import sys
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pandas as pd
 from fun import *
 
-#fileName='Data/241212_Dataset_Leopard24_IMU.csv'
-#fileName='Data/250312_Dataset_Pferd12.csv'
-fileName='Data/250318_Dataset_Eule3.csv'
+# fileName='Data/241212_Dataset_Leopard24_IMU.csv'
+# fileName='Data/250312_Dataset_Pferd12.csv'
+fileName = "Data/250318_Dataset_Eule3.csv"
 data = pd.read_csv(fileName, sep=",")
 
-#fig=plot_angle_vs_height(data.loc[:,'ElbowAngle'].values, data.loc[:, "RightHandZ"].values, data.loc[:, "Trial_ID"].values, 'ElbowAngle')
-#fig.show()
+# fig=plot_angle_vs_height(data.loc[:,'ElbowAngle'].values, data.loc[:, "RightHandZ"].values, data.loc[:, "Trial_ID"].values, 'ElbowAngle')
+# fig.show()
 
+
+# plot a heatmap of the drilling duration vs height
 def plot_trial_duration(data):
-    #selection = (data.loc[:, "RightHandZ"] < 500)
-    #data = data.loc[selection]
+    # selection = (data.loc[:, "RightHandZ"] < 500)
+    # data = data.loc[selection]
     trial_ids = data.loc[:, "Trial_ID"].values
-    
+
     fig = go.Figure()
     unique_trials = np.unique(trial_ids)
-    
+
     lengths = []
     height = []
     for trial_id in unique_trials:
         mask = trial_ids == trial_id
         lengths.append(trial_ids[mask].shape[0])
         height.append(np.mean(data.loc[mask, "RightHandZ"].values))
-    fig.add_trace(go.Histogram2d(
-        x=lengths,
-        y=height,
-        xbins=dict(size=20),
-        ybins=dict(size=20),
-        ))
+    fig.add_trace(
+        go.Histogram2d(
+            x=lengths,
+            y=height,
+            xbins=dict(size=20),
+            ybins=dict(size=20),
+        )
+    )
     fig.update_layout(
-        xaxis_title="Drilling Duration", 
-        yaxis_title="Height", 
-        title=f"Histogram of Drilling Duration", #Plot der Höhe gegen den {zielvariable}
+        xaxis_title="Drilling Duration",
+        yaxis_title="Height",
+        title=f"Histogram of Drilling Duration",  # Plot der Höhe gegen den {zielvariable}
         title_font=dict(size=30),
         xaxis=dict(title_font=dict(size=30), tickfont=dict(size=30)),
         yaxis=dict(title_font=dict(size=30), tickfont=dict(size=30)),
@@ -45,6 +50,8 @@ def plot_trial_duration(data):
     print(f"Average drilling duration: {average}")
     return fig
 
+
+# plot a heatmap of the drilling duration vs height but wiht plotly express
 def plot_trial_duration_express(data):
     trial_ids = data.loc[:, "Trial_ID"].values
     unique_trials = np.unique(trial_ids)
@@ -54,39 +61,50 @@ def plot_trial_duration_express(data):
         mask = trial_ids == trial_id
         lengths.append(trial_ids[mask].shape[0])
         height.append(np.mean(data.loc[mask, "RightHandZ"].values))
-    
-    fig = px.density_heatmap(x=lengths, y=height, nbinsx=30, nbinsy=30, marginal_x="histogram", marginal_y="histogram")
+
+    fig = px.density_heatmap(
+        x=lengths,
+        y=height,
+        nbinsx=30,
+        nbinsy=30,
+        marginal_x="histogram",
+        marginal_y="histogram",
+    )
     fig.update_layout(
-        xaxis_title="Drilling Duration", 
-        yaxis_title="Height", 
-        title=f"Histogram of Drilling Duration", #Plot der Höhe gegen den {zielvariable}
+        xaxis_title="Drilling Duration",
+        yaxis_title="Height",
+        title=f"Histogram of Drilling Duration",  # Plot der Höhe gegen den {zielvariable}
         title_font=dict(size=30),
         xaxis=dict(title_font=dict(size=30), tickfont=dict(size=30)),
         yaxis=dict(title_font=dict(size=30), tickfont=dict(size=30)),
     )
     return fig
 
+
+# plot a histogram of the drilling duration
 def plot_trial_duration_1dhist(data):
-    #selection = (data.loc[:, "RightHandZ"] < 500)
-    #data = data.loc[selection]
+    # selection = (data.loc[:, "RightHandZ"] < 500)
+    # data = data.loc[selection]
     trial_ids = data.loc[:, "Trial_ID"].values
-    
+
     fig = go.Figure()
     unique_trials = np.unique(trial_ids)
-    
+
     lengths = []
     for trial_id in unique_trials:
         mask = trial_ids == trial_id
         lengths.append(trial_ids[mask].shape[0])
-    fig.add_trace(go.Histogram(
-        x=lengths,
-        nbinsx=30,
-        marker=dict(color='blue'),
-    ))
+    fig.add_trace(
+        go.Histogram(
+            x=lengths,
+            nbinsx=30,
+            marker=dict(color="blue"),
+        )
+    )
     fig.update_layout(
         xaxis_title="Drilling Duration",
         yaxis_title="Count",
-        title=f"Histogram of Drilling Duration", #Plot der Höhe gegen den {zielvariable}
+        title=f"Histogram of Drilling Duration",  # Plot der Höhe gegen den {zielvariable}
         title_font=dict(size=30),
         xaxis=dict(title_font=dict(size=30), tickfont=dict(size=30)),
         yaxis=dict(title_font=dict(size=30), tickfont=dict(size=30)),
@@ -95,6 +113,7 @@ def plot_trial_duration_1dhist(data):
     average = np.mean(lengths)
     print(f"Average drilling duration: {average}")
     return fig
+
 
 fig = plot_trial_duration_1dhist(data)
 fig.show()
